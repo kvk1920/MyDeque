2ff4a00ea40297eeb4bb4b6dccc46ea124c1bb33
kvk1920 <34111966+kvk1920@users.noreply.github.com>
2017-12-02 03:33:22 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+#include "test_time.h"
+#include "test_data.h"
+
+int main(int argc, char* argv[]) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
